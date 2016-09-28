695e8810989befffbf8b71b587151bfd0bf0637f
Martin Krutina <gril51043@gmail.com>
2016-09-28 18:19:22 +0200

    initial commit, first draft

---
+++ manifests/container.pp
@@ -0,0 +1,114 @@
+# linux container
+define lxc::container (
+    $ensure = stopped,
+    $public_gw = undef,
+    $public_ip = undef,
+    $public_type = 'macvlan',
+    $public_link = 'mvlan0',
+    $private_ip = undef,
+    $private_gw = undef,
+    $private_type = 'macvlan',
+    $private_link = 'mvlan1',
+    $private_network = 'no',
+    $public_network = 'no',
+    $allow_tun = 'no',
+    $template = 'debian',
+    $release = 'jessie',
+    $lxcpath = '/var/lib/lxc',
+    $logfile = "${lxcpath}/${name}/${name}.log",
+    $autostart = '0',
+    $config_version = 'simple',
+  ) {
+
+  $private_ipaddr = split($private_ip,'/')
+  $common_config_name = $template
+
+# Container inicialization
+  exec { "lxc-create-${name}":
+    command => "lxc-create -n ${name} -t ${template} --lxcpath ${lxcpath} --logfile ${logfile} -- -r ${release}; echo \"nameserver ${private_gw}\" > ${lxcpath}/${name}/rootfs/etc/resolv.conf; echo \"127.0.0.1      localhost\" > ${lxcpath}/${name}/rootfs/etc/hosts; echo \"${private_ipaddr[0]}      ${name}\" >> ${lxcpath}/${name}/rootfs/etc/hosts; chroot ${lxcpath}/${name}/rootfs apt-get update ; chroot ${lxcpath}/${name}/rootfs apt-get install --assume-yes wget vim git iputils-ping ca-certificates python; chroot ${lxcpath}/${name}/rootfs wget https://apt.puppetlabs.com/puppetlabs-release-${release}.deb -O /tmp/puppetlabs-release-${release}.deb; chroot ${lxcpath}/${name}/rootfs dpkg -i /tmp/puppetlabs-release-${release}.deb; chroot ${lxcpath}/${name}/rootfs apt-get update ; chroot ${lxcpath}/${name}/rootfs apt-get install --assume-yes puppet-common;mkdir -p ${lxcpath}/${name}/rootfs/root/.ssh; cp /root/.ssh/authorized_keys ${lxcpath}/${name}/rootfs/root/.ssh/authorized_keys",
+    path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin:/usr/local/sbin:/sbin',
+    creates => "${lxcpath}/${name}/rootfs",
+    timeout => '900',
+    require => [Package['lxc'], Exec['reload-network']],
+    before  => File["${lxcpath}/${name}"]
+  }
+  file { "${lxcpath}/${name}":
+    ensure  => directory,
+    require => Package['lxc'],
+    before  => File["${lxcpath}/${name}/config"]
+  }
+  case $config_version {
+    'simple': {
+      file { "${lxcpath}/${name}/config":
+        ensure  => present,
+        mode    => '0644',
+        content => template('lxc/lxc-container.config.erb'),
+        notify  => Service["container-${name}"],
+        before  => File["${lxcpath}/${name}/fstab"]
+      }
+    }
+    'docker': {
+      file { "${lxcpath}/${name}/config":
+        ensure  => present,
+        mode    => '0644',
+        content => template('lxc/lxc-container-docker.config.erb'),
+        notify  => Service["container-${name}"],
+        before  => File["${lxcpath}/${name}/fstab"]
+      }
+    }
+    default: {
+      notify { "custom-${lxcpath}/${name}/config":
+        message => "no container config version for ${config_version} found, please create custom config file here: ${lxcpath}/${name}/config",
+      }
+    }
+  }
+  file { "${lxcpath}/${name}/fstab":
+    ensure  => file,
+    content => '',
+  }
+
+
+  # link target to default lxc folder
+  if $lxcpath != '/var/lib/lxc' {
+    file { "/var/lib/lxc/${name}":
+      ensure  => 'link',
+      target  => "${lxcpath}/${name}",
+      require => Exec["lxc-create-${name}"],
+    }
+  }
+
+  if $private_network == 'yes' {
+    service { "container-${name}-route":
+      ensure     => $ensure,
+      provider   => 'base',
+      enable     => true,
+      start      => "ip route add ${private_ipaddr[0]}/32 dev ${private_link}",
+      status     => "ip route list |grep -o -E \"${private_ipaddr[0]} dev ${private_link}  scope link\"",
+      stop       => "ip route del ${private_ipaddr[0]}/32 dev ${private_link}",
+      hasrestart => false,
+      hasstatus  => true,
+      require    => Exec["lxc-create-${name}"],
+    }
+    concat::fragment { "conatiner-route-persistent-${name}":
+      order   => "01_${name}",
+      target  => '/etc/network/if-up.d/local-containers',
+      content => template('lxc/local-containers.erb'),
+    }
+    Internal_network::Route <| |> {
+      device => "dev ${private_link}",
+    }
+  }
+
+  service { "container-${name}":
+    ensure     => $ensure,
+    provider   => 'base',
+    enable     => true,
+    start      => "lxc-start -n ${name} -d --logfile ${logfile}",
+    status     => "lxc-ls -f|grep -o -E \"${name} + RUNNIN\"",
+    stop       => "lxc-stop -n ${name} --logfile ${logfile}",
+    hasrestart => false,
+    hasstatus  => true,
+    require    => Exec["lxc-create-${name}"],
+  }
+
+}
---
+++ manifests/init.pp
@@ -0,0 +1,89 @@
+# lxc small class
+class lxc(
+  $public_bridge = undef,
+  $public_macvlan = undef,
+  $public_interface = undef,
+  $public_ip = undef,
+  $public_nm = undef,
+  $public_gw = undef,
+  $public_alias = [],
+  $public_vlan = 'no',
+  $public_vlanid = 0,
+  $private_bridge = undef,
+  $private_macvlan = undef,
+  $private_ip = undef,
+  $private_nm = undef,
+  $private_interface = undef,
+  $private_vlanid = 0,
+  $private_vlan = 'no',
+  $install_only = false
+  ) {
+
+  if $install_only == false {
+
+    $private_nm_cidr = netmask_to_masklen($private_nm)
+    file { '/etc/network/interfaces':
+      ensure  => present,
+      mode    => '0644',
+      content => template('lxc/lxc-network.config.erb'),
+      notify  => Exec['reload-network']
+    }
+
+    exec { 'reload-network':
+      command     => '/usr/sbin/service networking restart && /usr/bin/touch /etc/network/reload-network.lock',
+      creates     => '/etc/network/reload-network.lock',
+      user        => 'root',
+      refreshonly => true,
+      require     => [File['/etc/network/interfaces'], Package['bridge-utils', 'vlan', 'ifenslave']]
+    }
+
+    concat { '/etc/network/if-up.d/local-containers':
+      owner => 'root',
+      mode  => '0755',
+    }
+  }
+  else {
+    # dummy reload-network resorce if only install
+    exec { 'reload-network':
+      command     => '/usr/bin/touch /etc/network/reload-network.lock',
+      creates     => '/etc/network/reload-network.lock',
+      user        => 'root',
+      refreshonly => true,
+    }
+  }
+
+
+# instalation support debian 8
+  package { 'lxc':
+    ensure  => 'latest',
+    tag     => 'special',
+    require => Exec['apt-update-common-special']
+  }
+  package { 'bridge-utils':
+    ensure  => 'latest',
+    tag     => 'special',
+    require => Exec['apt-update-common-special']
+  }
+  package { 'libvirt-bin':
+    ensure  => 'latest',
+    tag     => 'special',
+    require => Exec['apt-update-common-special']
+  }
+  package { 'debootstrap':
+    ensure  => 'latest',
+    tag     => 'special',
+    require => Exec['apt-update-common-special']
+  }
+  package { 'vlan':
+    ensure  => 'latest',
+    tag     => 'special',
+    require => Exec['apt-update-common-special']
+  }
+  package { 'ifenslave':
+    ensure  => 'latest',
+    tag     => 'special',
+    require => Exec['apt-update-common-special']
+  }
+}
+
+
